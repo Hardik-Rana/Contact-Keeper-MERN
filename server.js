1d71d7ba3a1834a4278e3df78dde874070a6bec6
Hardik Rana <ranahardik2207@gmail.com>
2022-01-31 02:12:43 -0800

    try

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express =require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>res.send("hell"));
+app.get('/',(req,res)=>res.send("helgrgl"));
 
 const PORT= process.env.PORT || 5000;
 
