f4cc0f36a76a8e6bb90cbdf80f91f91bebcc5407
Hardik Rana <ranahardik2207@gmail.com>
2022-02-02 23:26:10 -0800

    Token stuff and all

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () =>{
+    
+    try {
+        await mongoose.connect(db);
+        console.log('MongoDB Connected');
+
+    } catch (error) {
+        
+        console.error(err.message);
+        process.exit(1);
+    }
+
+
+};
+
+module.exports= connectDB;
+
